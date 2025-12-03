99a25580a7ebb7cf12e5f8640811571ec77f2db0
Juan Manuel Gallego Toro <jmgallegot@emergiacc.com>
2025-12-03 12:14:25 -0500

    URL Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   apiUrlAutenticacionAD: 'https://systemservices.emergiacc.com/ApiGateway/ActiveDirectory/LoginFT?IdiomaNavegador=ES&Sistema=CANS',
   apiUrlDominio: 'https://systemservices.emergiacc.com/ApiGateway/ActiveDirectory/GetDomainsActiveDirectoryFromJsonAsync',
   apiUrlArquitecturaPuestos: 'https://systemapplications.emergiacc.com/ApiRestArquitecturaPuestos/api/v1',
-  apiUrlUpload: 'https://systemservices.emergiacc.com/EditorPdfApiRest/api/v1/pdf-file',
+  apiUrlUpload: 'https://systemservices.emergiacc.com/EditorPdfApiRest/api/v1',
   apiUrlConvertidor:'https://systemservices.emergiacc.com/EditorPdfApiRest/api/v1/convertir',
   apiUrlEditor:'https://systemservices.emergiacc.com/EditorPdfApiRest/api/v1/herramientas/pdf',
 };
